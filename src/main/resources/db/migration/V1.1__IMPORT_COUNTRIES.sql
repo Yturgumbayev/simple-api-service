8c524d9de81458c712dd1a7b9bfbba5e17afcaa5
erlanturg <yturgumbayev@beeline.kz>
2022-05-20 09:41:43 +0600

    import info for countries

---
+++ src/main/resources/db/migration/V1.1__IMPORT_COUNTRIES.sql
@@ -0,0 +1,1 @@
+insert into public.countries (country_name, capital, population, is_nato_member) values ('Казахстан', 'Нур-Султан', 19504321, false);
--- src/main/resources/db/migration/V1__CREATE_TABLE.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE TABLE countries (
-                      id serial NOT NULL,
-                      country_name varchar(256) not null unique,
-                      capital varchar(256) not null,
-                      population BIGINT NOT NULL,
-                      is_nato_member BOOLEAN NOT NULL,
-                      PRIMARY KEY (id)
-);
